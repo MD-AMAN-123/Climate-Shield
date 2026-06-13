1b0af9ebb8e94399d4fbbdec9d7d13f1616faa99
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-13 17:09:59 +0530

    Update chatbot.py

--- AI-chatbot/chatbot.py
+++ AI-chatbot/chatbot.py
@@ -225,9 +225,9 @@ def generate_response(user_input, context_summary=""):
     # ======================================
     # BASIC CONVERSATION
     # ======================================
-    user_words=user.strip().split()
+    user_words=user_input.strip().lower().split()
     hello_keywords=["hello","hi","hey"]
-    if any(keyword in user_words for keyword im hello_keywords) or user.strip in hello_keywords:
+    if any(keyword in hello_keywords for keyword in user_words) or user_input.lower().strip() in hello_keywords:
 
         return random.choice([
 
@@ -236,7 +236,7 @@ def generate_response(user_input, context_summary=""):
             "Greetings from Climate Shield 🌍"
         ])
 
-    if "how are you" in user:
+    if "how are you" in user_words:
 
         return random.choice([
 
@@ -245,7 +245,7 @@ def generate_response(user_input, context_summary=""):
             "All systems operational."
         ])
 
-    if any(word in user for word in
+    if any(word in user_words for word in
            ["thank", "thanks"]):
 
         return random.choice([
@@ -256,7 +256,7 @@ def generate_response(user_input, context_summary=""):
         ])
 
     exit_keywords=["bye","quit","exit","goodbye"]
-    if any(keyword in user_words for keyword in exit_keywords) or user.strip().lower() in exit_keywords:
+    if any(keyword in exit_keywords for keyword in user_words) or user_input.lower().strip() in exit_keywords:
         return random.choice([
             "Goodbye 🌍 Stay safe.",
             "Take care,"
